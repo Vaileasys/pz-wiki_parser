8b2d7d5e3eaeecf935f6526e042ebe18e077d918
Calvy <isaacmusic14@gmail.com>
2024-08-10 21:29:55 +1000

    Add header parsing

--- article.py
+++ article.py
@@ -3,6 +3,80 @@ import re
 import sys
 
 
+def build_header(category, skill_type):
+    # Define the category dictionary
+    category_dict = {
+        "Weapon": "use_skill_type",
+        "Tool/Weapon": "use_skill_type",
+        "Tool / Weapon": "use_skill_type",
+        "Weapon - Crafted": "use_skill_type",
+        "Explosives": "{{Header|Project Zomboid|Items|Weapons|Explosives}}",
+        "Ammo": "{{Header|Project Zomboid|Items|Weapons|Firearms|Ammo}}",
+        "Material": "{{Header|Project Zomboid|Items|Materials}}",
+        "Materials": "{{Header|Project Zomboid|Items|Materials}}",
+        "Accessory": "{{Header|Project Zomboid|Items|Clothing}}",
+        "Clothing": "{{Header|Project Zomboid|Items|Clothing}}",
+        "Tool": "{{Header|Project Zomboid|Items|Equipment|Tools}}",
+        "Tools": "{{Header|Project Zomboid|Items|Equipment|Tools}}",
+        "Junk": "{{Header|Project Zomboid|Items|Miscellaneous items|Junk}}",
+        "Mole": "{{Header|Project Zomboid|Items|Miscellaneous items|Plush toys}}",
+        "Raccoon": "{{Header|Project Zomboid|Items|Miscellaneous items|Plush toys}}",
+        "Squirrel": "{{Header|Project Zomboid|Items|Miscellaneous items|Plush toys}}",
+        "Fox": "{{Header|Project Zomboid|Items|Miscellaneous items|Plush toys}}",
+        "Badger": "{{Header|Project Zomboid|Items|Miscellaneous items|Plush toys}}",
+        "Beaver": "{{Header|Project Zomboid|Items|Miscellaneous items|Plush toys}}",
+        "Bunny": "{{Header|Project Zomboid|Items|Miscellaneous items|Plush toys}}",
+        "Hedgehog": "{{Header|Project Zomboid|Items|Miscellaneous items|Plush toys}}",
+        "Electronics": "{{Header|Project Zomboid|Items|Electronics}}",
+        "Weapon Part": "{{Header|Project Zomboid|Items|Weapons|Firearms|Weapon parts}}",
+        "WeaponPart": "{{Header|Project Zomboid|Items|Weapons|Firearms|Weapon parts}}",
+        "Light Source": "{{Header|Project Zomboid|Items|Equipment|Light sources}}",
+        "LightSource": "{{Header|Project Zomboid|Items|Equipment|Light sources}}",
+        "Literature": "{{Header|Project Zomboid|Items|Literature}}",
+        "Paint": "{{Header|Project Zomboid|Items|Materials}}",
+        "First Aid": "{{Header|Project Zomboid|Items|Medical items}}",
+        "FirstAid": "{{Header|Project Zomboid|Items|Medical items}}",
+        "Fishing": "{{Header|Project Zomboid|Game mechanics|Character|Skills|Fishing}}",
+        "Communication": "{{Header|Project Zomboid|Items|Electronics|Communications}}",
+        "Communications": "{{Header|Project Zomboid|Items|Electronics|Communications}}",
+        "Camping": "{{Header|Project Zomboid|Items|Equipment|Camping}}",
+        "Cartography": "{{Header|Project Zomboid|Items|Literature|Cartography}}",
+        "Cooking": "{{Header|Project Zomboid|Game mechanics|Crafting|Cooking}}",
+        "Entertainment": "{{Header|Project Zomboid|Items|Electronics|Entertainment}}",
+        "Food": "{{Header|Project Zomboid|Items|Food}}",
+        "Household": "{{Header|Project Zomboid|Items|Miscellaneous items|Household}}",
+        "Appearance": "{{Header|Project Zomboid|Items|Miscellaneous items|Appearance}}"
+    }
+
+    # Define the skill type dictionary
+    skill_type_dict = {
+        "Long Blade": "{{Header|Project Zomboid|Items|Weapons|Melee weapons|Long blade weapons}}",
+        "Short Blade": "{{Header|Project Zomboid|Items|Weapons|Melee weapons|Short blade weapons}}",
+        "Long Blunt": "{{Header|Project Zomboid|Items|Weapons|Melee weapons|Long blunt weapons}}",
+        "Short Blunt": "{{Header|Project Zomboid|Items|Weapons|Melee weapons|Short blunt weapons}}",
+        "Spear": "{{Header|Project Zomboid|Items|Weapons|Melee weapons|Spears}}",
+        "Axe": "{{Header|Project Zomboid|Items|Weapons|Melee weapons|Axes}}",
+        "Aiming": "{{Header|Project Zomboid|Items|Weapons|Firearms}}",
+        "Firearm": "{{Header|Project Zomboid|Items|Weapons|Firearms}}"
+    }
+
+    # Strip square brackets and anything inside them from skill type
+    skill_type = re.sub(r'\[\[(?:[^\|\]]*\|)?([^\|\]]+)\]\]', r'\1', skill_type).strip()
+
+    # Check if category matches
+    if category in category_dict:
+        if category_dict[category] == "use_skill_type":
+            # Check if skill type matches
+            if skill_type in skill_type_dict:
+                return skill_type_dict[skill_type]
+            else:
+                return "{{Header|Project Zomboid|Items|Weapons}}"
+        else:
+            return category_dict[category]
+    else:
+        return "{{Header|Project Zomboid|Items}}"  # Default header
+
+
 def process_file(file_path, output_dir):
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
@@ -27,6 +101,14 @@ def process_file(file_path, output_dir):
 
     infobox_version = version_match.group(1)
 
+    # Extract item_id
+    item_id_match = re.search(r'\|item_id\s*=\s*(.+)', infobox)
+    if not item_id_match:
+        print(f"Item ID not found in {file_path}")
+        return
+
+    item_id = item_id_match.group(1).strip()
+
     # Extract name
     name_match = re.search(r'\|name\s*=\s*(.+)', infobox)
     if not name_match:
@@ -35,10 +117,6 @@ def process_file(file_path, output_dir):
 
     name = name_match.group(1).strip()
     lowercase_name = name.lower()
-
-    # Extract item_id
-    item_id_match = re.search(r'\|item_id\s*=\s*(.+)', infobox)
-    item_id = item_id_match.group(1).strip() if item_id_match else ""
 
     # Extract category
     category_match = re.search(r'\|category\s*=\s*(.+)', infobox)
@@ -51,11 +129,14 @@ def process_file(file_path, output_dir):
     # Determine if 'A' should be 'An'
     article = 'An' if lowercase_name[0] in 'aeiou' else 'A'
 
+    # Build the header
+    header = build_header(category, skill_type)
+
     # Create the output content
     body_content = assemble_body(lowercase_name, os.path.basename(file_path), name, item_id, category, skill_type, infobox)
 
     new_content = f"""
-{{{{Header|Project Zomboid|Items}}}}
+{header}
 {{{{Page version|{infobox_version}}}}}
 {{{{Autogenerated|B42}}}}
 {infobox}
@@ -64,11 +145,11 @@ def process_file(file_path, output_dir):
 {body_content}
 """
 
-    # Sanitize the name to create a valid filename
-    safe_name = re.sub(r'[^\w\-_\. ]', '_', name) + '.txt'
+    # Sanitize the item_id to create a valid filename
+    safe_item_id = re.sub(r'[^\w\-_\. ]', '_', item_id) + '.txt'
 
     # Determine the new file path
-    new_file_path = os.path.join(output_dir, safe_name)
+    new_file_path = os.path.join(output_dir, safe_item_id)
 
     # Write the new content to the output file
     try:
@@ -110,6 +191,7 @@ def generate_condition(name, category, skill_type, infobox):
     condition_lower_chance = condition_lower_chance_match.group(1)
 
     return f"""The {name} has a maximum condition of {condition_max}. Its rate of degradation is influenced by the {skill_type} and [[maintenance]] [[skill]]s. The chance of losing [[durability]] can be simplified to the following formula: <code>1 in (35 + maintenanceMod &times; 2)</code>. Where "maintenanceMod" is calculated using the {skill_type} and maintenance skills.<br>
+
 {{{{Durability weapon|{condition_lower_chance}|{condition_max}|skill={skill_type}}}}}"""
 
 
@@ -133,6 +215,7 @@ def generate_location(original_filename, infobox_name, item_id):
                 with open(file_path, 'r', encoding='utf-8') as file:
                     return file.read().strip()
             except Exception as e:
+
                 print(f"Error reading {file_path}: {e}")
     return ""
 
@@ -161,7 +244,7 @@ def generate_code(item_id):
 
 
 def generate_see_also(name, category):
-    see_also_list = "* [[]]\n* [[]]\n* [[]]"
+    see_also_list = "*{{ll|Placeholder}}\n*{{ll|Placeholder}}\n*{{ll|Placeholder}}"
     category_list = ["Weapon", "Tool", "Clothing", "Food"]
 
     if category in category_list:
