c1c8da3169565338ba1028a878d57b0d728368ed
Calvy <isaacmusic14@gmail.com>
2024-09-03 11:55:10 +1000

    Fix trailing whitespace

--- scripts/codesnip.py
+++ scripts/codesnip.py
@@ -2,6 +2,7 @@ import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from core import utility
 
+
 def extract_code_snippet(line_number, lines):
     item_code = ""
     for i in range(line_number, len(lines)):
@@ -10,9 +11,9 @@ def extract_code_snippet(line_number, lines):
             break
     return item_code, line_number + 1
 
-def save_snippet(item_name, item_code, line, source, version, output_dir):
-    def write_snippet(name):
-        formatted_code = f"""{{{{CodeSnip
+
+def write_snippet(name, cleaned_code, line, source, version, output_dir):
+    formatted_code = f"""{{{{CodeSnip
   | lang = java
   | line = true
   | start = {line}
@@ -20,15 +21,23 @@ def save_snippet(item_name, item_code, line, source, version, output_dir):
   | retrieved = true
   | version = {version}
   | code =
-{item_code.strip()} 
+{cleaned_code}
 }}}}"""
-        with open(os.path.join(output_dir, f'{name}.txt'), 'w') as f:
-            f.write(formatted_code)
 
-    write_snippet(item_name)
+    formatted_code_cleaned = "\n".join([line.rstrip() for line in formatted_code.splitlines()])
+    with open(os.path.join(output_dir, f'{name}.txt'), 'w') as f:
+        f.write(formatted_code_cleaned)
+
+
+def save_snippet(item_name, item_code, line, source, version, output_dir):
+    # Remove trailing whitespace
+    cleaned_code = "\n".join([line.rstrip() for line in item_code.splitlines()])
+    write_snippet(item_name, cleaned_code, line, source, version, output_dir)
+
     if item_name.endswith("TEXTURE_TINT") or item_name.endswith("DECAL_TINT"):
         base_name = item_name.replace("TEXTURE_TINT", "").replace("DECAL_TINT", "")
-        write_snippet(base_name)
+        write_snippet(base_name, cleaned_code, line, source, version, output_dir)
+
 
 def process_file(file_path, version, output_dir):
     try:
@@ -41,6 +50,7 @@ def process_file(file_path, version, output_dir):
                 save_snippet(item_name, item_code, line_num, os.path.basename(file_path), version, output_dir)
     except Exception as e:
         print(f"Error processing file {file_path}: {e}")
+
 
 def main():
     version = utility.version
@@ -56,6 +66,7 @@ def main():
         futures = [executor.submit(process_file, file_path, version, output_dir) for file_path in files]
         for future in as_completed(futures):
             pass
+
 
 if __name__ == "__main__":
     main()
