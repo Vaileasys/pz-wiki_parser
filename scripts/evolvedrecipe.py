ccd1c4eca67a0afc015e207216065d4323646edf
Vaileasys <kevin.kb.banks@gmail.com>
2024-09-07 13:58:06 +1000

    rewrite for new item parser

--- scripts/evolvedrecipe.py
+++ scripts/evolvedrecipe.py
@@ -1,87 +1,60 @@
 import csv
-import script_parser
-import re
-import os
+from scripts.parser import item_parser
+
 
 def main():
-    script_parser.init()
-
-    input_file_path = 'output/logging/parsed_item_data.txt'
     output_file_path = 'output/evolved_recipe_values.csv'
-
-    # Check if the input file exists
-    if not os.path.exists(input_file_path):
-        print(f"Error: The file {input_file_path} does not exist.")
-        return
+    parsed_item_data = item_parser.get_item_data()
+    # Store only unique 'EvolvedRecipe' values
+    evolved_recipes = set()
 
     try:
-        # Remove comments from the file content
-        with open(input_file_path, 'r') as file:
-            content = file.read()
-        content = re.sub(r'/\*.*?\*/', '', content, flags=re.DOTALL)
-        content = re.sub(r'//.*', '', content)
+        # Iterate through each item getting all unique 'EvolvedRecipe' values
+        for item_id, item_data in parsed_item_data.items():
+            property_data = item_data.get("EvolvedRecipe")
+            if property_data:
+                evolved_recipes.update(property_data.keys())
+    
+        # Sort alphabetically then add 'Name' and 'FoodType' columns
+        evolved_recipes = sorted(evolved_recipes)
+        evolved_recipes = ['Name'] + ['FoodType'] + evolved_recipes
+        
+    except Exception as e:
+        print(f"An error occurred when getting the heading: {e}")
 
-        lines = content.splitlines()
-        items_dict = {}
-        current_item_name = None
-        current_food_type = None
+    try:
+        with open(output_file_path, 'w', newline='') as file:
+            writer = csv.writer(file)
+            # Write header row
+            writer.writerow(list(evolved_recipes))
 
-        # Process each line
-        for line in lines:
-            line = line.strip()
+            # Iterate through each item getting the 'EvolvedRecipe' and 'FoodType'
+            for item_id, item_data in parsed_item_data.items():
+                evolved_recipe = item_data.get('EvolvedRecipe', None)
+                food_type = item_data.get('FoodType', 'NoExplicit')
+                
+                # Don't include item if it doesn't have an evolved_recipe
+                if evolved_recipe is None:
+                    continue
+                
+                # Get item_name from the item_id [compatibility]
+                item_name = item_id.split('.')[1]
+                row = [item_name] + [food_type]
 
-            # Check if the line defines a new item
-            if line.startswith('item'):
-                current_item_name = line.split()[1]
-                items_dict[current_item_name] = {'FoodType': None}
-
-            # Check if the line contains a FoodType and not EvolvedRecipeName
-            if line.startswith('FoodType =') and current_item_name:
-                current_food_type = extract_food_type(line)
-                items_dict[current_item_name]['FoodType'] = current_food_type
-
-            # Check if the line contains an EvolvedRecipe and not EvolvedRecipeName
-            if line.startswith('EvolvedRecipe =') and current_item_name:
-                evolved_recipe_values = extract_evolved_recipe_values(line)
-
-                # Populate the dictionary only if evolved recipes exist
-                if evolved_recipe_values:
-                    items_dict[current_item_name].update(evolved_recipe_values)
-
-        # Filter out items without any evolved recipes
-        items_dict = {k: v for k, v in items_dict.items() if len(v) > 1}
-
-        # Extract all unique recipe names and sort them alphabetically
-        all_recipe_names = sorted({recipe for values in items_dict.values() for recipe in values.keys() if recipe != 'FoodType'})
-
-        # Write the results to the CSV file
-        with open(output_file_path, 'w', newline='') as csvfile:
-            csvwriter = csv.writer(csvfile)
-
-            header_row = ['Name', 'FoodType'] + all_recipe_names
-            csvwriter.writerow(header_row)
-
-            for item_name, recipes in items_dict.items():
-                row = [item_name, recipes.get('FoodType', '')] + [recipes.get(recipe, '') for recipe in all_recipe_names]
-                csvwriter.writerow(row)
-
-        print("\nSuccessfully extracted evolved recipe values and food types.")
+                # Append the item's evolved_recipes to the row in the correct column
+                for key in evolved_recipes[2:]:
+                    value = evolved_recipe.get(key, None)
+                    # Convert attributes (e.g. 'Cooked') back to scripts format [compatibility]
+                    if isinstance(value, list):
+                        value = '|'.join(value)
+                    row.append(value if value is not None else '')
+                
+                writer.writerow(row)
+                print("\nSuccessfully extracted evolved recipe values and food types.")
 
     except Exception as e:
         print(f"An error occurred: {e}")
 
-def extract_food_type(line):
-    return line.split('=', 1)[1].strip()
-
-def extract_evolved_recipe_values(line):
-    evolved_recipes = line.split('=', 1)[1].strip()
-    recipe_values = {}
-    matches = re.findall(r"'([^']+):([^']+)'", evolved_recipes)
-
-    for recipe, value in matches:
-        recipe_values[recipe.strip()] = value.strip()
-
-    return recipe_values
 
 if __name__ == '__main__':
     main()
