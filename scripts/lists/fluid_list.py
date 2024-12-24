24dd504fc4c9ab3598783940ac37c7e34bdb4dd7
Vaileasys <kevin.kb.banks@gmail.com>
2024-12-24 22:39:43 +1100

    header translations

--- scripts/lists/fluid_list.py
+++ scripts/lists/fluid_list.py
@@ -6,20 +6,20 @@ from scripts.parser import fluid_parser
 #TODO: add translations
 
 HEADER = """{| class="wikitable theme-red sortable sticky-column" style="text-align: center;"
-! Name
-! Color
-! style="width: 50px;" | [[File:Moodle_Icon_Tired.png|link=|Fatigue]]
-! [[File:Moodle_Icon_Hungry.png|link=|Hunger]]
-! [[File:Moodle_Icon_Stressed.png|link=|Stress]]
-! [[File:Moodle_Icon_Thirsty.png|link=|Thirst]]
-! [[File:Moodle_Icon_Unhappy.png|link=|Unhappiness]]
-! [[File:Fire_01_1.png|32px|link=Nutrition#Calories|Calories]]
-! [[File:Wheat.png|32px|link=Nutrition#Carbohydrates|Carbohydrates]]
-! [[File:Butter.png|32px|link=Nutrition#Fat|Fat]]
-! [[File:Steak.png|32px|link=Nutrition#Proteins|Proteins]]
-! [[File:WhiskeyFull.png|32px|link=Alcohol|Alcohol]]
-! style="width: 50px;" | [[File:SkullPoison.png|link=|Poison]]
-! Fluid ID
+! <<name>>
+! <<color>>
+! [[File:Status_Hunger.png|32px|link=|<<hunger>>]]
+! [[File:Status_Thirst.png|32px|link=|<<thirst>>]]
+! [[File:Fire_01_1.png|32px|link=|<<calories>>]]
+! [[File:Wheat.png|32px|link=|<<carbohydrates>>]]
+! [[File:Butter.png|32px|link=|<<fat>>]]
+! [[File:Steak.png|32px|link=|<<proteins>>]]
+! [[File:SkullPoison.png|link=|<<poison>>]]
+! [[File:Mood_Drunk.png|32px|link=|<<alcohol>>]]
+! [[File:Mood_Sleepy.png|32px|link=|<<fatigue>>]]
+! [[File:Mood_Stressed.png|32px|link=|<<stress>>]]
+! [[File:Mood_Sad.png|32px|link=|<<unhappiness>>]]
+! <<fluid_id>>
 |-"""
 
 
@@ -32,7 +32,8 @@ def write_fluids_to_file(items, file_name):
 
     output_file = os.path.join(output_dir, f"{file_name}.txt")
     with open(output_file, "w", encoding="utf-8") as file:
-        file.write(HEADER)
+        translated_header = translate.get_wiki_translation(HEADER)
+        file.write(translated_header)
         for item in items:
             for value in item.values():
                 # Replace '0' with '-'
@@ -54,7 +55,6 @@ def get_fluids():
 
     for fluid_id, fluid_data in fluid_parser.get_fluid_data().items():
         fluid_id = f"Base.{fluid_id}" # Assume fluid ID is in the 'Base' module
-        print(fluid_id)
 
         display_name = fluid_data.get('DisplayName', 'Fluid')
         display_name_prefix = "Fluid_Name_"
@@ -67,9 +67,6 @@ def get_fluids():
             name = f"[[{name_en} (fluid)/{translate.language_code}|{name}]]"
         else:
             name = f"[[{name} (fluid)|{name}]]"
-
-
-        print(name)
 
         color = fluid_data.get('ColorReference', fluid_data.get('Color', [0.0, 0.0, 0.0])),
 
@@ -88,31 +85,30 @@ def get_fluids():
         if 'Poison' in fluid_data:
             poison_data = fluid_data['Poison']
             poison = poison_data.get('maxEffect', '-')
-        else: 
+        else:
             poison = '-'
 
         fluid_values = {
             "name": name,
             "color": color_str,
-            "fatigue_change": properties_data.get('fatigueChange', '-'),
             "hunger_change": properties_data.get('hungerChange', '-'),
-            "stress_change": properties_data.get('stressChange', '-'),
             "thirst_change": properties_data.get('thirstChange', '-'),
-            "unhappy_change": properties_data.get('unhappyChange', '-'),
             "calories": properties_data.get('calories', '-'),
             "carbohydrates": properties_data.get('carbohydrates', '-'),
             "lipids": properties_data.get('lipids', '-'),
             "proteins": properties_data.get('proteins', '-'),
+            "poison": poison,
             "alcohol": properties_data.get('alcohol', '-'),
+            "fatigue_change": properties_data.get('fatigueChange', '-'),
+            "stress_change": properties_data.get('stressChange', '-'),
+            "unhappy_change": properties_data.get('unhappyChange', '-'),
             # No fluids with the following data yet
 #            "flu_reduction": properties_data.get('fluReduction', '-'),
 #            "pain_reduction": properties_data.get('painReduction', '-'),
 #            "endurance_change": properties_data.get('enduranceChange', '-'),
 #            "food_sickness_reduction": properties_data.get('foodSicknessReduction', '-'),
-            "poison": poison,
             "fluid_id": fluid_id
         }
-        print("----")
 
         fluids.append(fluid_values)
 
