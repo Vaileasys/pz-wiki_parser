7c490b4cdd8538c534efb99fbb8f6afc795648db
CalvyPZ <isaacmusic14@gmail.com>
2025-01-16 11:51:10 +1100

    Add lua file to list

--- scripts/core/setup.py
+++ scripts/core/setup.py
@@ -114,7 +114,8 @@ def copy_lua_files(media_dir: str) -> None:
         'SpecialItemData_Magazines.lua',
         'SpecialItemData_Misc.lua',
         'SpecialItemData_Photos.lua',
-        'camping_fuel.lua'
+        'camping_fuel.lua',
+        'MainCreationMethods.lua'
     ])
 
     destination_dir = Path('resources/lua')
