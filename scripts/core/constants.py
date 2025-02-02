2a989bd94a7e942420a5ce49ed54d5136bf48efa
Vaileasys <kevin.kb.banks@gmail.com>
2025-02-02 17:34:06 +1100

    Add lua path

--- scripts/core/constants.py
+++ scripts/core/constants.py
@@ -2,6 +2,7 @@
 OUTPUT_PATH = "output"
 RESOURCE_PATH = "resources"
 DATA_PATH = "data"
+LUA_PATH = RESOURCE_PATH + "/lua"
 
 # TQDM progress bar format - 'bar_format' variable
 PBAR_FORMAT = "{l_bar}{bar:30}{r_bar}"
