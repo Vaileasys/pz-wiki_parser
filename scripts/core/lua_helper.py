aa81dc6e141a76bf23b767aa99876675b70d1ad3
Vaileasys <kevin.kb.banks@gmail.com>
2025-02-05 22:07:06 +1100

    Can define folder path, single table, and multiple lua strings

--- scripts/core/lua_helper.py
+++ scripts/core/lua_helper.py
@@ -7,44 +7,69 @@ def load_lua_file(lua_files: list[str], lua_runtime: LuaRuntime=None, dependenci
     """
     Loads and executes a Lua file in the given Lua runtime.
 
-    :param lua_files (list[str]): List of Lua file paths to load and execute. A single file path can also be provided as a string.
+    :param lua_files (str or list[str]): List of Lua file paths to load and execute. A single file path can also be provided as a string.
     :param lua_runtime (LuaRuntime, optional): An existing Lua runtime instance. If not provided, a new Lua runtime will be initialised.
     :param dependencies (list[str], optional): List of additional Lua dependency files to load before the main files (LUA_PATH is already included).
     :param inject_lua (str, optional): Lua code to inject directly into the runtime before executing any files. Useful for adding stubs or fallbacks.
-    :return: The Lua runtime after executing the files.
+    :return (LuaRuntime): The Lua runtime after executing the files.
     """
+
+    def get_lua_files(paths):
+        """Returns a list of Lua files from given paths, including subdirectories if paths are directories.
+
+        :param paths (str or list[str]): The path to get all files from.
+        :return (list[str]): List of lua file paths.
+        """
+        files_list = []
+
+        # Ensure paths is a list
+        if isinstance(paths, str):
+            paths = [paths]
+
+        for path in paths:
+            path = os.path.join(LUA_PATH, path)
+            try:
+                # Is directory
+                if os.path.isdir(path):
+                    for root, dirs, files in os.walk(path):
+                        for file in files:
+                            if file.endswith('.lua'):
+                                full_path = os.path.join(root, file)
+                                files_list.append(full_path)
+                # Is lua file
+                elif os.path.isfile(path) and path.endswith('.lua'):
+                    files_list.append(path)
+                else:
+                    print(f"Warning: {path} is not a valid lua file or directory.")
+            except (PermissionError, FileNotFoundError) as e:
+                print(f"Error accessing {path}: {e}")
+
+        return files_list
+
     # Initialise new Lua runtime environment if one wasn't provided
     if not lua_runtime:
         lua_runtime = LuaRuntime(unpack_returned_tuples=True)
 
     # Inject Lua into the runtime environemnt. Can be used for fallbacks, such as creating stubs.
     if inject_lua:
+        if isinstance(inject_lua, list):
+            inject_lua = "\n\n".join(inject_lua)
         lua_runtime.execute(inject_lua)
-    
-    try:
-        # Load extra Lua files if provided
-        if dependencies:
-            for dependency in dependencies:
-                dependency_path = os.path.join(LUA_PATH, dependency)
-                if not os.path.isfile(dependency_path):
-                    raise FileNotFoundError(f"Dependency file not found: {dependency_path}")
 
+    try:
+        # Load dependencies
+        if dependencies:
+            dependency_files = get_lua_files(dependencies)
+            for dependency_path in dependency_files:
                 with open(dependency_path, 'r', encoding='utf-8') as f:
                     try:
                         lua_runtime.execute(f.read())
                     except LuaError as e:
-                        raise LuaError(f"Error executing extra Lua file '{dependency_path}': {e}")
-                    
-        if isinstance(lua_files, str):
-            lua_files = [lua_files]
+                        raise LuaError(f"Error executing dependency '{dependency_path}': {e}")
         
-        for lua_file in lua_files:
-            lua_file_path = os.path.join(LUA_PATH, lua_file)
-
-            # Load the Lua files
-            if not os.path.isfile(lua_file_path):
-                raise FileNotFoundError(f"Lua file not found: {lua_file_path}")
-
+        # Load main lua files
+        lua_files_list = get_lua_files(lua_files)
+        for lua_file_path in lua_files_list:
             with open(lua_file_path, 'r', encoding='utf-8') as lua_file:
                 try:
                     lua_runtime.execute(lua_file.read())
@@ -121,6 +146,9 @@ def parse_lua_tables(lua_runtime: LuaRuntime, tables: list[str] = None) -> dict:
     }
 
     if tables:
+        if isinstance(tables, str):
+            tables = [tables]
+
         for table_name in tables:
             try:
                 lua_table = lua_runtime.eval(table_name)
@@ -138,8 +166,29 @@ def parse_lua_tables(lua_runtime: LuaRuntime, tables: list[str] = None) -> dict:
 ## ------------------------- EVERYTHING BENEATH HERE IS FOR TESTING/EXAMPLE ------------------------- ##
 
 
-# Inject lua: Foraging - initialises and stubs Events table
-test_lua_fallback = ("""
+LUA_CLUTTER_TABLES = ("""
+    ClutterTables = ClutterTables or setmetatable({}, {
+        __index = function(_, key)
+            return setmetatable({}, {
+                __index = function(_, inner_key)
+                    return function() return tostring(inner_key) end
+                end
+            })
+        end
+    })
+""")
+LUA_BAGS_AND_CONTAINERS = ("""
+    BagsAndContainers = BagsAndContainers or setmetatable({}, {
+        __index = function(_, key)
+            return setmetatable({}, {
+                __index = function(_, inner_key)
+                    return function() return tostring(inner_key) end
+                end
+            })
+        end
+    })
+""")
+LUA_EVENTS = ("""
     local function fallback()
         return setmetatable({}, {
             __index = function(_, key)
@@ -162,8 +211,9 @@ test_lua_fallback = ("""
         end
     })
 """)
+# Inject lua: Foraging - initialises and stubs Events table
 if __name__ == "__main__":
-    lua_runtime = load_lua_file("forageSystem.lua", inject_lua=test_lua_fallback)
+    lua_runtime = load_lua_file("forageSystem.lua", inject_lua=LUA_EVENTS)
     parsed_data = parse_lua_tables(lua_runtime)
     utility.save_cache(parsed_data, "foraging_2.json")
 
@@ -186,34 +236,7 @@ if __name__ == "__main__":
     utility.save_cache(parsed_data, "attached_weapon_definitions.json")
 
 # Inject lua and multiple files: All distributions - initialises ClutterTables and BagsAndContainers tables
-distributions_lua = ("""
-    ClutterTables = ClutterTables or setmetatable({}, {
-        __index = function(_, key)
-            return setmetatable({}, {
-                __index = function(_, inner_key)
-                    return function() return tostring(inner_key) end
-                end
-            })
-        end
-    })
-
-    BagsAndContainers = BagsAndContainers or setmetatable({}, {
-        __index = function(_, key)
-            return setmetatable({}, {
-                __index = function(_, inner_key)
-                    return function() return tostring(inner_key) end
-                end
-            })
-        end
-    })
-""")
 DISTRIBUTION_LUA_FILES = [
-    "ProceduralDistributions.lua",
-    "VehicleDistributions.lua",
-    "VehicleDistribution_GloveBoxJunk.lua",
-    "VehicleDistribution_SeatJunk.lua",
-    "VehicleDistribution_TrunkJunk.lua",
-    "Distribution_BinJunk.lua",
     "Distribution_BagsAndContainers.lua",
     "Distribution_BinJunk.lua",
     "Distribution_ClosetJunk.lua",
@@ -221,13 +244,15 @@ DISTRIBUTION_LUA_FILES = [
     "Distribution_DeskJunk.lua",
     "Distribution_ShelfJunk.lua",
     "Distribution_SideTableJunk.lua",
+    "ProceduralDistributions.lua",
+    "Distributions.lua",
 ]
 if __name__ == "__main__":
-    lua_runtime = load_lua_file("Distributions.lua", dependencies=DISTRIBUTION_LUA_FILES, inject_lua=distributions_lua) # Initialise and load lua files into lua environment
+    lua_runtime = load_lua_file(DISTRIBUTION_LUA_FILES) # Initialise and load lua files into lua environment
     parsed_data = parse_lua_tables(lua_runtime) # Get all the tables and convert to python data
     utility.save_cache(parsed_data, "distributions_all.json") # Save to json file
 
-copy_table_fallback = ("""
+LUA_COPY_TABLE = ("""
     function copyTable(tbl)
         local copy = {}
         for k, v in pairs(tbl) do
@@ -240,24 +265,26 @@ copy_table_fallback = ("""
         return copy
     end
 """)
-ANIMAL_LUA_FILES = [
-    "animal/ChickenDefinitions.lua",
-    "animal/CowDefinitions.lua",
-    "animal/DeerDefinitions.lua",
-    "animal/MouseDefinitions.lua",
-    "animal/PigDefinitions.lua",
-    "animal/RabbitDefinitions.lua",
-    "animal/RaccoonDefinitions.lua",
-    "animal/RatDefinitions.lua",
-    "animal/SheepDefinitions.lua",
-    "animal/TurkeyDefinitions.lua",
-]
 if __name__ == "__main__":
-    lua_runtime = load_lua_file(ANIMAL_LUA_FILES, inject_lua=copy_table_fallback)
-    parsed_data = parse_lua_tables(lua_runtime)
+    lua_runtime = load_lua_file("animal", inject_lua=LUA_COPY_TABLE)
+    parsed_data = parse_lua_tables(lua_runtime, tables=["AnimalDefinitions"])
     utility.save_cache(parsed_data, "animal_definitions.json")
 
+# Loads lua files, inject multiple strings and define a specific table (this one is a local table, so we get the index with [1])
+# Load order matters. We can either modify the order of the list, or use 'dependencies'
+VEHICLE_DIST_FILES = [
+    "VehicleDistribution_GloveBoxJunk.lua",
+    "VehicleDistribution_SeatJunk.lua",
+    "VehicleDistribution_TrunkJunk.lua",
+    "VehicleDistributions.lua",
+]
+if __name__ == "__main__":
+    lua_runtime = load_lua_file(VEHICLE_DIST_FILES, inject_lua=[LUA_EVENTS, LUA_CLUTTER_TABLES])
+    parsed_data = parse_lua_tables(lua_runtime, tables=["VehicleDistributions[1]"])
+    parsed_data = dict(sorted(parsed_data["VehicleDistributions[1]"].items()))
+    utility.save_cache(parsed_data, "vehicles.json")
 
+# Table: Parse a specific table
 TABLES = ["PrintMediaDefinitions"]
 FILES_LIST = [
     'SpecialItemData_Books.lua',
