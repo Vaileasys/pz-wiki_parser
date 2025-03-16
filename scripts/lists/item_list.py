b6ccfce437612722874ce3f710fd938dd876ea07
Vaileasys <kevin.kb.banks@gmail.com>
2025-03-16 19:36:53 +1100

    Add highlighted rows with missing id and clean up

--- scripts/lists/item_list.py
+++ scripts/lists/item_list.py
@@ -2,8 +2,7 @@ from tqdm import tqdm
 import os
 from scripts.parser import item_parser
 from scripts.core import translate, utility, logger
-
-pbar_format = "{l_bar}{bar:30}{r_bar}"
+from scripts.core.constants import (PBAR_FORMAT, OUTPUT_PATH)
 
 filters = {
 #    'ExampleItemPrefix': (True,),
@@ -29,45 +28,60 @@ def translate_category(category, property="DisplayCategory"):
     return cat_translated
 
 
-def write_to_output(sorted_items):
+def write_to_output(items_data):
     language_code = translate.get_language_code()
-    output_dir = f'output/{language_code}/item_list/'
-    output_file = f'item_list_{language_code.upper()}.txt'
+    output_dir = f'{OUTPUT_PATH}/{language_code}/item_list/'
+    output_file = f'item_list.txt'
     os.makedirs(output_dir, exist_ok=True)
     output_path = os.path.join(output_dir, output_file)
 
-    for display_category in sorted_items:
-        sorted_items[display_category] = sorted(sorted_items[display_category], key=lambda x: x[1])
+    content = []
+
+    lc_subpage = ""
+    if language_code != "en":
+        lc_subpage = f"/{language_code}"
+
+    for display_category in sorted(items_data.keys()):
+        translated_header = translate.get_wiki_translation(HEADER)
+        content.append(f"=={display_category}==")
+        content.append('{| class="wikitable theme-blue"')
+        content.append(f"{translated_header}")
+
+        for item in items_data[display_category]:
+            page_name = item["page_name"]
+            item_name = item["item_name"]
+            icons = item["icon"]
+            item_id = item["item_id"]
+            id_rec = item["id_rec"]
+
+            icons_image = ' '.join([f"[[File:{icon}|32x32px]]" for icon in icons])
+            item_link = f"[[{page_name}]]"
+
+            if language_code != "en" or page_name != item_name:
+                item_link = f"[[{page_name}{lc_subpage}|{item_name}]]"
+
+            if id_rec:
+                content.append('|-')
+            else:
+                content.append('|- title="ID missing in infobox" style="background-color: var(--background-color-warning-subtle); color:red;"')
+                item_id = item_id + "*"
+
+            content.append(f"| {icons_image} || {item_link} || {item_id}")
+        content.append("|}\n")
 
     with open(output_path, 'w', encoding='utf-8') as file:
-
-        lc_subpage = ""
-        if language_code != "en":
-            lc_subpage = f"/{language_code}"
-
-        for display_category in sorted(sorted_items.keys()):
-            translated_header = translate.get_wiki_translation(HEADER)
-            file.write(f"=={display_category}==\n")
-            file.write('{| class="wikitable theme-blue"\n')
-            file.write(f"{translated_header}\n")
-            for page_name, translated_item_name, icons, item_id in sorted_items[display_category]:
-                icons_image = ' '.join([f"[[File:{icon}|32x32px]]" for icon in icons])
-                item_link = f"[[{page_name}]]"
-
-                if language_code != "en" or page_name != translated_item_name:
-                    item_link = f"[[{page_name}{lc_subpage}|{translated_item_name}]]"
-                file.write(f"|-\n| {icons_image} || {item_link} || {item_id}\n")
-            file.write("|}\n\n")
+        file.write("\n".join(content))
 
     print(f"Output saved to {output_path}")
 
 
-def item_list():
-    sorted_items = {}
+def generate_item_list():
+    items_data = {}
     parsed_item_data = item_parser.get_item_data()
 
-    with tqdm(total=len(parsed_item_data), desc="Processing items", bar_format=pbar_format, unit=" items") as pbar:
+    with tqdm(total=len(parsed_item_data), desc="Processing items", bar_format=PBAR_FORMAT, unit=" items") as pbar:
         for item_id, item_data in parsed_item_data.items():
+            id_rec = False
             pbar.set_postfix_str(f"Processing: {item_data.get('Type', 'Unknown')} ({item_id[:30]})")
             # Check if 'DisplayCategory' property exists for the item
             if 'DisplayCategory' in item_data:
@@ -82,6 +96,9 @@ def item_list():
                 page_name = utility.get_page(item_id)
                 if page_name == 'Unknown':
                     page_name = translated_item_name
+                else:
+                    id_rec = True
+
                 if translate.get_language_code() != 'en':
                     translated_item_name = utility.get_name(item_id, item_data)
                 
@@ -109,12 +126,28 @@ def item_list():
                 # Add item to the sorted dictionary
                 if display_category is None:
                     display_category = 'Unknown'
-                if display_category not in sorted_items:
-                    sorted_items[display_category] = []
-                sorted_items[display_category].append((page_name, translated_item_name, icon, item_id))
+
+                if display_category not in items_data:
+                    items_data[display_category] = []
+
+                processed_item = {
+                    "page_name": page_name,
+                    "item_name": translated_item_name,
+                    "icon": icon,
+                    "item_id": item_id,
+                    "id_rec": id_rec
+                    }
+
+                items_data[display_category].append(processed_item)
+
+#                items_data[display_category].append((page_name, translated_item_name, icon, item_id, id_rec))
             pbar.update(1)
         pbar.bar_format = f"Items processed."
-    write_to_output(sorted_items)
+
+    for display_category in items_data:
+        items_data[display_category] = sorted(items_data[display_category], key=lambda x: x["item_name"])
+
+    write_to_output(items_data)
 
 
 def filters_tree():
@@ -143,16 +176,16 @@ def filters_tree():
 
 
 def main():
-    while True:
-        user_input = input("1: Run script\n2: Set up a filter\nQ: Quit\n> ").lower()
-        if user_input == "1":
-            item_list()
-            return
-        elif user_input == "2":
-            filters_tree()
-        elif user_input == "q":
-            return
-
+#    while True:
+#        user_input = input("1: Run script\n2: Set up a filter\nQ: Quit\n> ").lower()
+#        if user_input == "1":
+#            generate_item_list()
+#            return
+#        elif user_input == "2":
+#            filters_tree()
+#        elif user_input == "q":
+#            return
+    generate_item_list()
 
 if __name__ == "__main__":
     main()
