6b884eb15cfc1388aaf811ba1a4d95f77ebb6be4
Vaileasys <kevin.kb.banks@gmail.com>
2025-03-25 21:27:56 +1100

    Added optional traceback message and path getter

--- scripts/core/logger.py
+++ scripts/core/logger.py
@@ -1,8 +1,14 @@
 import os
 from tqdm import tqdm
+import traceback
+from scripts.core.constants import OUTPUT_PATH
 
+LOG_PATH = f"{OUTPUT_PATH}\\logging"
+DEF_FILE = "log.txt"
 is_first_log = True
 
+def get_log_path(file_name=DEF_FILE):
+    return os.path.join(LOG_PATH, file_name)
 
 # Initialise log file erasing existing contents
 def init_log_file(file_name="log.txt"):
@@ -13,20 +19,40 @@ def init_log_file(file_name="log.txt"):
 
 
 # Used to log important info to a log file
-def write(message, print_bool="False", file_name="log.txt"):
+def write(message, print_bool=False, file_name=DEF_FILE, exception=None):
     global is_first_log
-    file_name = "output/logging/" + file_name
+    file_name = get_log_path(file_name)
 
     # If this is the first log, initialise the log file
     if is_first_log:
         init_log_file(file_name)
         is_first_log = False
 
-    if print_bool is True:
+    if exception is not None:
+        tb = traceback.extract_tb(exception.__traceback__)
+        filename, lineno, func, text = tb[-1]
+
+        # Get the current working directory
+        base_dir = os.path.abspath(os.getcwd())
+        abs_path = os.path.abspath(filename)
+
+        # Get the relative path from the working dir
+        try:
+            rel_path = os.path.relpath(abs_path, base_dir)
+        except ValueError:
+            # If relpath fails just use filename
+            rel_path = os.path.basename(filename)
+
+        post_message = f": {exception} (File {rel_path}, line {lineno}, in {func})"
+    else:
+        post_message = ""
+        
+
+    if print_bool:
         if tqdm._instances:
-            tqdm.write(message)
+            tqdm.write(f"{message}{post_message}")
         else:
-            print(message)
+            print(f"{message}{post_message}")
 
     with open(file_name, 'a') as file:
-        file.write(f"{message}\n")
+        file.write(f"{message}{post_message}\n")
