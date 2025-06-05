b10f3f2894b254b28fe7ce29dc05b374e5a4a233
CalvyPZ <isaacmusic14@gmail.com>
2025-06-05 22:58:45 +1000

    Add new workstations

--- scripts/recipes/craft_recipes.py
+++ scripts/recipes/craft_recipes.py
@@ -538,6 +538,9 @@ def process_workstation(recipe: dict, build_data: dict) -> str:
         "domekiln": "Kiln - Dome",
         "kilnlarge": "Advanced Kiln",
         "kilnsmall": "Primitive Kiln",
+        "Heckling": "Heckle Comb",
+        "Rippling": "Ripple Comb",
+        "Scutching": "Scutching Board",
     }
 
     for tag_identifier in tag_list:
