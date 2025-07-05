c4cef36ae242383b316e930ccfafe84d3a0cff35
Vaileasys <kevin.kb.banks@gmail.com>
2025-07-05 17:25:02 +1000

    Add/update properties with defaults

--- scripts/objects/foraging.py
+++ scripts/objects/foraging.py
@@ -3,6 +3,7 @@ from scripts.core.cache import load_cache
 from scripts.core.constants import DATA_DIR
 from scripts.parser import distribution_parser
 from scripts.objects.item import Item
+from scripts.utils import util
 
 DISTRIBUTIONS_DIR = os.path.join(DATA_DIR, "distributions")
 FORAGING_CACHE_FILE = "foraging.json"
@@ -60,9 +61,9 @@ class Foraging:
         """Returns the raw value from this item's foraging data."""
         return self._data.get(key, default)
 
-    def _translate_months(self, months: list):
+    def _translate_month(self, month: int):
         from scripts.core.language import Translate
-        return [Translate.get(f"Sandbox_StartMonth_option{m}") for m in months]
+        return Translate.get(f"Sandbox_StartMonth_option{month}")
 
     @property
     def id(self) -> str:
@@ -81,70 +82,125 @@ class Foraging:
         return Item(self.type) if Item.exists(self.type) else None
 
     @property
-    def skill(self) -> int | None:
-        return self._data.get("skill")
+    def skill(self) -> int:
+        return self._data.get("skill", 0)
 
     @property
-    def xp(self) -> int | None:
-        return self._data.get("xp")
+    def xp(self) -> int:
+        return self._data.get("xp", 1)
 
     @property
-    def zones(self) -> dict | None:
-        return self._data.get("zones")
+    def perks(self) -> list[str]:
+        return self._data.get("perks", ["PlantScavenging"])
+
+    @property
+    def zones(self) -> dict:
+        zones_default = {
+            "Forest": 1,
+            "DeepForest": 1,
+            "Vegitation": 1,
+            "FarmLand": 1,
+            "Farm": 1,
+            "TrailerPark": 1,
+            "TownZone": 1,
+            "Nav": 1,
+            "ForagingNav": 1
+        }
+        return self._data.get("zones", zones_default)
 
     @property
     def categories(self) -> list[str]:
         if not hasattr(self, "_categories"):
-            raw = self._data.get("categories", {})
+            raw = self._data.get("categories", {"1": "Junk"})
             self._categories = list(raw.values()) if isinstance(raw, dict) else []
         return self._categories
 
     @property
-    def min_count(self) -> int | None:
-        return self._data.get("minCount")
+    def min_count(self) -> int:
+        return self._data.get("minCount", 1)
 
     @property
-    def max_count(self) -> int | None:
-        return self._data.get("maxCount")
+    def max_count(self) -> int:
+        return self._data.get("maxCount", 1)
     
     @property
     def spawn_funcs(self) -> dict:
         return self._data.get("spawnFuncs", {})
 
+    def _get_month_links(self, months):
+        month_links = []
+
+        for month in months:
+            if month in (12, 1, 2):
+                anchor = "Winter"
+            elif month in (3, 4, 5):
+                anchor = "Spring"
+            elif month in (6, 7, 8):
+                anchor = "Summer"
+            elif month in (9, 10, 11):
+                anchor = "Autumn"
+            else:
+                anchor = "Seasons"
+        
+            month_links.append(util.link("Weather", self._translate_month(month), anchor=anchor))
+
+        return month_links
+
+    @property
+    def months_raw(self) -> list[int]:
+        if not hasattr(self, "_months_raw"):
+            raw = self._data.get("months")
+            values = raw.values() if isinstance(raw, dict) else [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+            self._months_raw = sorted(set(values))
+        return self._months_raw
+
     @property
     def months(self) -> list[int]:
-        raw = self._data.get("months", {})
-        return self._translate_months(sorted(set(raw.values()))) if isinstance(raw, dict) else []
+        return self._get_month_links(self.months_raw)
+
+    @property
+    def bonus_months_raw(self) -> list[int]:
+        if not hasattr(self, "_bonus_months_raw"):
+            raw = self._data.get("bonsuMonths", {})
+            values = raw.values() if isinstance(raw, dict) else []
+            self._bonus_months_raw = sorted(set(values))
+        return self._bonus_months_raw
 
     @property
     def bonus_months(self) -> list[int]:
-        raw = self._data.get("bonusMonths", {})
-        return self._translate_months(sorted(set(raw.values()))) if isinstance(raw, dict) else []
+        return self._get_month_links(self.bonus_months_raw)
+
+    @property
+    def malus_months_raw(self) -> list[int]:
+        if not hasattr(self, "_malus_months_raw"):
+            raw = self._data.get("malusMonths", {})
+            values = raw.values() if isinstance(raw, dict) else []
+            self._malus_months_raw = sorted(set(values))
+        return self._malus_months_raw
 
     @property
     def malus_months(self) -> list[int]:
-        raw = self._data.get("malusMonths", {})
-        return self._translate_months(sorted(set(raw.values()))) if isinstance(raw, dict) else []
+        return self._get_month_links(self.malus_months_raw)
 
     @property
-    def day_chance(self) -> int | None:
-        return self._data.get("dayChance")
+    def day_chance(self) -> int:
+        return self._data.get("dayChance", 0)
 
     @property
-    def night_chance(self) -> int | None:
-        return self._data.get("nightChance")
+    def night_chance(self) -> int:
+        return self._data.get("nightChance", 0)
 
     @property
-    def snow_chance(self) -> int | None:
-        return self._data.get("snowChance")
+    def snow_chance(self) -> int:
+        return self._data.get("snowChance", 0)
 
     @property
-    def rain_chance(self) -> int | None:
-        return self._data.get("rainChance")
+    def rain_chance(self) -> int:
+        return self._data.get("rainChance", 0)
 
     @property
-    def item_size_modifier(self) -> float | None:
-        return self._data.get("itemSizeModifier")
+    def item_size_modifier(self) -> float:
+        return self._data.get("itemSizeModifier", 0.0)
 
     @property
     def is_item_override_size(self) -> bool:
@@ -152,27 +208,27 @@ class Foraging:
 
     @property
     def force_outside(self) -> bool:
-        return self._data.get("forceOutside", False)
+        return self._data.get("forceOutside", True)
 
     @property
     def can_be_above_floor(self) -> bool:
         return self._data.get("canBeAboveFloor", False)
 
     @property
-    def poison_chance(self) -> int | None:
-        return self._data.get("poisonChance")
+    def poison_chance(self) -> int:
+        return self._data.get("poisonChance", 0)
 
     @property
-    def poison_power_min(self) -> int | None:
-        return self._data.get("poisonPowerMin")
+    def poison_power_min(self) -> int:
+        return self._data.get("poisonPowerMin", 0)
 
     @property
-    def poison_power_max(self) -> int | None:
-        return self._data.get("poisonPowerMax")
+    def poison_power_max(self) -> int:
+        return self._data.get("poisonPowerMax", 0)
 
     @property
-    def poison_detection_level(self) -> int | None:
-        return self._data.get("poisonDetectionLevel")
+    def poison_detection_level(self) -> int:
+        return self._data.get("poisonDetectionLevel", 0)
 
     @property
     def alt_world_texture(self) -> str | None:
